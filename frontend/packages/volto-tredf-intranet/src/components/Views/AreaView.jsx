4cbb3d7869cec012b7dfadb0e8e9518f239bef4d
Érico Andrei <ericof@gmail.com>
2025-08-15 16:37:43 -0300

    Exercício 27: Altera exibição do tipo Area para incluir informações de endereço

--- frontend/packages/volto-tredf-intranet/src/components/Views/AreaView.jsx
+++ frontend/packages/volto-tredf-intranet/src/components/Views/AreaView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getBaseUrl } from '@plone/volto/helpers/Url/Url';
 import { Container } from '@plone/components';
 import ContactInfo from '../ContactInfo/ContactInfo';
+import EnderecoInfo from '../EnderecoInfo/EnderecoInfo';
 import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
 
 const AreaView = (props) => {
@@ -12,6 +13,7 @@ const AreaView = (props) => {
     <Container id="page-document" className="view-wrapper area-view">
       <RenderBlocks {...props} path={path} />
       <ContactInfo content={content} />
+      <EnderecoInfo content={content} />
     </Container>
   );
 };
