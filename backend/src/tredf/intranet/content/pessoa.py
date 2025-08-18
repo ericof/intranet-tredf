5e17411c29717c55d7312421d6e1c3bc3b3efcf7
Érico Andrei <ericof@gmail.com>
2025-08-18 16:49:55 -0300

    Exercício 32: Adiciona campo cargo

--- backend/src/tredf/intranet/content/pessoa.py
+++ backend/src/tredf/intranet/content/pessoa.py
@@ -1,10 +1,19 @@
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from tredf.intranet import _
+from zope import schema
 from zope.interface import implementer
 
 
 class IPessoa(model.Schema):
     """Definição de uma Pessoa."""
+
+    cargo = schema.Choice(
+        title=_("Cargo"),
+        description=_("Selecione qual o cargo dessa pessoa no TRE-DF."),
+        vocabulary="tredf.intranet.vocabulary.cargos",
+        required=False,
+    )
 
 
 @implementer(IPessoa)
